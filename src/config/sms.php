f77122f664a619abbbe6e62bd8078933ab1449a3
Usama Arslan <usama.arslan@softpyramid.com>
2022-12-23 13:20:30 +0500

    Updated base url

--- src/config/sms.php
+++ src/config/sms.php
@@ -2,5 +2,6 @@
 
 return [
     'apiKey' => env('API_KEY', null),
-    'apiSecret' => env('API_SECRET', null)
+    'apiSecret' => env('API_SECRET', null),
+    'baseUrl' => "https://messaging.projects.softpyramid.com/api/v1/"
 ];
