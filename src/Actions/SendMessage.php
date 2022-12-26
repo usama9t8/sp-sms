891de9a538a5cab95835f8784fa26731e85887d2
Usama Arslan <usama.arslan@softpyramid.com>
2022-12-26 12:31:34 +0500

    Minor fix

--- src/Actions/SendMessage.php
+++ src/Actions/SendMessage.php
@@ -10,7 +10,7 @@ class SendMessage
     public function execute($params)
     {
         $baseUrl = isset($params['local']) && $params['local'] ? config('sms.localUrl') : config('sms.baseUrl');
-        $res = Http::post(config('sms.baseUrl') . 'sendSms', [
+        $res = Http::post($baseUrl . 'sendSms', [
             'api_key' => config('sms.apiKey'),
             'api_secret' => config('sms.apiSecret'),
             'receiver_phone' => $params['phone'],
